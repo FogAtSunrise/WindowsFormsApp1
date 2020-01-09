616f407d7d3da8f208a6b5a674641c0b4cd9032f
Rici-Tiki <hjr462@mail.ru>
2020-01-09 20:46:06 +0700

    ооооооооооооо

--- WindowsFormsApp1/Form2.Designer.cs
+++ WindowsFormsApp1/Form2.Designer.cs
@@ -28,90 +28,199 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.panel_DAY = new System.Windows.Forms.Panel();
+            this.button_user = new System.Windows.Forms.Button();
+            this.main_menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.создатьОтчетToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.настройкиToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.помощьToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.button_DAY = new System.Windows.Forms.Button();
+            this.button_WEEK = new System.Windows.Forms.Button();
+            this.button_MONTH = new System.Windows.Forms.Button();
+            this.button1_user = new System.Windows.Forms.Button();
+            this.panel_menu_dlya_paneli_day = new System.Windows.Forms.Panel();
+            this.button_ON_opoveschalku = new System.Windows.Forms.Button();
+            this.button_OFF_opoveschalku = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.button4 = new System.Windows.Forms.Button();
+            this.panel_gde_budut_zapisi = new System.Windows.Forms.Panel();
+            this.label_date = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.panel_DAY.SuspendLayout();
+            this.main_menuStrip1.SuspendLayout();
+            this.panel_menu_dlya_paneli_day.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // panel_DAY
             // 
-            this.button1.Location = new System.Drawing.Point(727, 110);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.panel_DAY.Controls.Add(this.dateTimePicker1);
+            this.panel_DAY.Controls.Add(this.label_date);
+            this.panel_DAY.Controls.Add(this.panel_gde_budut_zapisi);
+            this.panel_DAY.Controls.Add(this.panel_menu_dlya_paneli_day);
+            this.panel_DAY.Location = new System.Drawing.Point(0, 67);
+            this.panel_DAY.Name = "panel_DAY";
+            this.panel_DAY.Size = new System.Drawing.Size(937, 458);
+            this.panel_DAY.TabIndex = 0;
             // 
-            // button2
+            // button_user
             // 
-            this.button2.Location = new System.Drawing.Point(551, 74);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 28);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button_user.Location = new System.Drawing.Point(945, 4);
+            this.button_user.Name = "button_user";
+            this.button_user.Size = new System.Drawing.Size(120, 24);
+            this.button_user.TabIndex = 1;
+            this.button_user.Text = "Пользователь";
+            this.button_user.UseVisualStyleBackColor = true;
+            // 
+            // main_menuStrip1
+            // 
+            this.main_menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.main_menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.создатьОтчетToolStripMenuItem1,
+            this.настройкиToolStripMenuItem1,
+            this.помощьToolStripMenuItem1});
+            this.main_menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.main_menuStrip1.Name = "main_menuStrip1";
+            this.main_menuStrip1.Size = new System.Drawing.Size(937, 30);
+            this.main_menuStrip1.TabIndex = 2;
+            this.main_menuStrip1.Text = "menuStrip1";
+            // 
+            // создатьОтчетToolStripMenuItem1
+            // 
+            this.создатьОтчетToolStripMenuItem1.Name = "создатьОтчетToolStripMenuItem1";
+            this.создатьОтчетToolStripMenuItem1.Size = new System.Drawing.Size(119, 26);
+            this.создатьОтчетToolStripMenuItem1.Text = "Создать отчет";
+            // 
+            // настройкиToolStripMenuItem1
+            // 
+            this.настройкиToolStripMenuItem1.Name = "настройкиToolStripMenuItem1";
+            this.настройкиToolStripMenuItem1.Size = new System.Drawing.Size(98, 26);
+            this.настройкиToolStripMenuItem1.Text = "Настройки";
+            // 
+            // помощьToolStripMenuItem1
+            // 
+            this.помощьToolStripMenuItem1.Name = "помощьToolStripMenuItem1";
+            this.помощьToolStripMenuItem1.Size = new System.Drawing.Size(83, 26);
+            this.помощьToolStripMenuItem1.Text = "Помощь";
+            // 
+            // button_DAY
+            // 
+            this.button_DAY.Location = new System.Drawing.Point(0, 31);
+            this.button_DAY.Name = "button_DAY";
+            this.button_DAY.Size = new System.Drawing.Size(306, 36);
+            this.button_DAY.TabIndex = 3;
+            this.button_DAY.Text = "ДЕНЬ";
+            this.button_DAY.UseVisualStyleBackColor = true;
+            // 
+            // button_WEEK
+            // 
+            this.button_WEEK.Location = new System.Drawing.Point(304, 31);
+            this.button_WEEK.Name = "button_WEEK";
+            this.button_WEEK.Size = new System.Drawing.Size(339, 36);
+            this.button_WEEK.TabIndex = 4;
+            this.button_WEEK.Text = "НЕДЕЛЯ";
+            this.button_WEEK.UseVisualStyleBackColor = true;
+            // 
+            // button_MONTH
+            // 
+            this.button_MONTH.Location = new System.Drawing.Point(639, 31);
+            this.button_MONTH.Name = "button_MONTH";
+            this.button_MONTH.Size = new System.Drawing.Size(298, 36);
+            this.button_MONTH.TabIndex = 5;
+            this.button_MONTH.Text = "МЕСЯЦ";
+            this.button_MONTH.UseVisualStyleBackColor = true;
+            // 
+            // button1_user
+            // 
+            this.button1_user.Location = new System.Drawing.Point(811, 0);
+            this.button1_user.Name = "button1_user";
+            this.button1_user.Size = new System.Drawing.Size(114, 28);
+            this.button1_user.TabIndex = 6;
+            this.button1_user.Text = "Пользователь";
+            this.button1_user.UseVisualStyleBackColor = true;
+            // 
+            // panel_menu_dlya_paneli_day
+            // 
+            this.panel_menu_dlya_paneli_day.Controls.Add(this.button3);
+            this.panel_menu_dlya_paneli_day.Controls.Add(this.button_OFF_opoveschalku);
+            this.panel_menu_dlya_paneli_day.Controls.Add(this.button_ON_opoveschalku);
+            this.panel_menu_dlya_paneli_day.Location = new System.Drawing.Point(664, 0);
+            this.panel_menu_dlya_paneli_day.Name = "panel_menu_dlya_paneli_day";
+            this.panel_menu_dlya_paneli_day.Size = new System.Drawing.Size(272, 458);
+            this.panel_menu_dlya_paneli_day.TabIndex = 0;
+            // 
+            // button_ON_opoveschalku
+            // 
+            this.button_ON_opoveschalku.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button_ON_opoveschalku.Location = new System.Drawing.Point(0, 0);
+            this.button_ON_opoveschalku.Name = "button_ON_opoveschalku";
+            this.button_ON_opoveschalku.Size = new System.Drawing.Size(272, 56);
+            this.button_ON_opoveschalku.TabIndex = 0;
+            this.button_ON_opoveschalku.Text = "Включить все оповещения";
+            this.button_ON_opoveschalku.UseVisualStyleBackColor = true;
+            // 
+            // button_OFF_opoveschalku
+            // 
+            this.button_OFF_opoveschalku.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button_OFF_opoveschalku.Location = new System.Drawing.Point(0, 51);
+            this.button_OFF_opoveschalku.Name = "button_OFF_opoveschalku";
+            this.button_OFF_opoveschalku.Size = new System.Drawing.Size(272, 56);
+            this.button_OFF_opoveschalku.TabIndex = 1;
+            this.button_OFF_opoveschalku.Text = "Выключить все оповещения";
+            this.button_OFF_opoveschalku.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(333, 96);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button3.Location = new System.Drawing.Point(0, 104);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 28);
+            this.button3.Size = new System.Drawing.Size(272, 56);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
+            this.button3.Text = "Удалить все";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // checkBox1
+            // panel_gde_budut_zapisi
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(441, 101);
-            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(98, 21);
-            this.checkBox1.TabIndex = 3;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.panel_gde_budut_zapisi.Location = new System.Drawing.Point(4, 51);
+            this.panel_gde_budut_zapisi.Name = "panel_gde_budut_zapisi";
+            this.panel_gde_budut_zapisi.Size = new System.Drawing.Size(660, 404);
+            this.panel_gde_budut_zapisi.TabIndex = 1;
             // 
-            // checkBox2
+            // label_date
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(325, 185);
-            this.checkBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(98, 21);
-            this.checkBox2.TabIndex = 4;
-            this.checkBox2.Text = "checkBox2";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.label_date.AutoSize = true;
+            this.label_date.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_date.Location = new System.Drawing.Point(108, 10);
+            this.label_date.Name = "label_date";
+            this.label_date.Size = new System.Drawing.Size(67, 29);
+            this.label_date.TabIndex = 2;
+            this.label_date.Text = "Дата";
             // 
-            // button4
+            // dateTimePicker1
             // 
-            this.button4.Location = new System.Drawing.Point(115, 74);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(100, 28);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.dateTimePicker1.Location = new System.Drawing.Point(264, 17);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(131, 22);
+            this.dateTimePicker1.TabIndex = 3;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(937, 519);
+            this.Controls.Add(this.button1_user);
+            this.Controls.Add(this.button_MONTH);
+            this.Controls.Add(this.button_WEEK);
+            this.Controls.Add(this.button_DAY);
+            this.Controls.Add(this.button_user);
+            this.Controls.Add(this.main_menuStrip1);
+            this.Controls.Add(this.panel_DAY);
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form2";
             this.Text = "Form2";
+            this.panel_DAY.ResumeLayout(false);
+            this.panel_DAY.PerformLayout();
+            this.main_menuStrip1.ResumeLayout(false);
+            this.main_menuStrip1.PerformLayout();
+            this.panel_menu_dlya_paneli_day.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -119,11 +228,22 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel_DAY;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label_date;
+        private System.Windows.Forms.Panel panel_gde_budut_zapisi;
+        private System.Windows.Forms.Panel panel_menu_dlya_paneli_day;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button_OFF_opoveschalku;
+        private System.Windows.Forms.Button button_ON_opoveschalku;
+        private System.Windows.Forms.Button button_user;
+        private System.Windows.Forms.MenuStrip main_menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem создатьОтчетToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem настройкиToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem помощьToolStripMenuItem1;
+        private System.Windows.Forms.Button button_DAY;
+        private System.Windows.Forms.Button button_WEEK;
+        private System.Windows.Forms.Button button_MONTH;
+        private System.Windows.Forms.Button button1_user;
     }
 }
