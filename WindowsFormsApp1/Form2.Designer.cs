688a68694332a4d2467ea0d50d9006b215627030
Rici-Tiki <hjr462@mail.ru>
2020-01-09 21:42:00 +0700

    ввв

--- WindowsFormsApp1/Form2.Designer.cs
+++ WindowsFormsApp1/Form2.Designer.cs
@@ -45,13 +45,24 @@
             this.button_WEEK = new System.Windows.Forms.Button();
             this.button_MONTH = new System.Windows.Forms.Button();
             this.button1_user = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.panel_DAY.SuspendLayout();
             this.panel_menu_dlya_paneli_day.SuspendLayout();
             this.main_menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel_DAY
             // 
+            this.panel_DAY.Controls.Add(this.panel1);
             this.panel_DAY.Controls.Add(this.dateTimePicker1);
             this.panel_DAY.Controls.Add(this.label_date);
             this.panel_DAY.Controls.Add(this.panel_gde_budut_zapisi);
@@ -85,7 +96,7 @@
             this.panel_gde_budut_zapisi.Name = "panel_gde_budut_zapisi";
             this.panel_gde_budut_zapisi.Size = new System.Drawing.Size(660, 404);
             this.panel_gde_budut_zapisi.TabIndex = 1;
-           
+            // 
             // panel_menu_dlya_paneli_day
             // 
             this.panel_menu_dlya_paneli_day.Controls.Add(this.button3);
@@ -202,6 +213,82 @@
             this.button1_user.Text = "Пользователь";
             this.button1_user.UseVisualStyleBackColor = true;
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dateTimePicker2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(937, 458);
+            this.panel1.TabIndex = 4;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePicker2.Location = new System.Drawing.Point(210, 11);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(271, 34);
+            this.dateTimePicker2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(108, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Дата";
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(4, 51);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(660, 404);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Location = new System.Drawing.Point(664, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(272, 458);
+            this.panel3.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(0, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(272, 56);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Удалить все";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Location = new System.Drawing.Point(0, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(272, 56);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Выключить все оповещения";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button4.Location = new System.Drawing.Point(0, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(272, 56);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Включить все оповещения";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -222,6 +309,9 @@
             this.panel_menu_dlya_paneli_day.ResumeLayout(false);
             this.main_menuStrip1.ResumeLayout(false);
             this.main_menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -246,5 +336,13 @@
         private System.Windows.Forms.Button button_WEEK;
         private System.Windows.Forms.Button button_MONTH;
         private System.Windows.Forms.Button button1_user;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
     }
 }
